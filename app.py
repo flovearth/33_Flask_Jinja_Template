bd331a8aa7aea0691ea0cdbbd216be46e27af6f3
flovearth <flovearth@protonmail.com>
2020-08-28 19:40:30 +0000

    files added.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return render_template("index.html", number1 = 12, number2 = 34)
+
+@app.route("/about")
+def second():
+    return render_template("about.html", hazirlayan = "Feyzullah SARI")
+
+if __name__ == "__main__":
+    app.run(debug = True)
